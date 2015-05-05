932bf7b139c7ecc489869d521a4c25b169e51c10
roycol <police0311@gmail.com>
2015-05-05 09:56:57 -0400

    last commit

---
+++ WebApplication1/scripts/game.js
@@ -0,0 +1,13 @@
+﻿Class Person{
+    constructor(){       
+    }
+    public sayHello(){
+        console.log("hello world");
+    }
+}
+
+fuction init(){
+    var myPerson = new Person();
+    myPerson.sayHello();
+}
+
